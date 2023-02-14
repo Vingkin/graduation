d8f93fbdba1578f5efb6a694b70276a1307d7c3e
Vingkin <1830053226@qq.com>
2023-02-14 16:26:06 +0800

    smscode

--- src/views/system/detection.vue
+++ src/views/system/detection.vue
@@ -1,13 +1,60 @@
 <template>
-
+  <el-row>
+    <el-col :span="8">
+      <el-card :body-style="{ padding: '0px' }">
+        <img src="https://vingkin-1304361015.cos.ap-shanghai.myqcloud.com/%E7%94%9F%E6%B4%BB/1.jpg"
+             class="image">
+        <div style="padding: 14px;">
+          <span>好吃的汉堡</span>
+          <div class="bottom clearfix">
+            <time class="time">{{ currentDate }}</time>
+            <el-button type="text" class="button">操作按钮</el-button>
+          </div>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
-  name: 'detection'
+  name: 'detection',
+  data() {
+    return {
+      currentDate: new Date()
+    }
+  }
 }
 </script>
 
 <style scoped>
+.time {
+  font-size: 13px;
+  color: #999;
+}
 
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
 </style>
